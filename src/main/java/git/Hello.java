3ee602cbed813f2c0b0c88058ac7a740a38391b3
zjh_atguigu <zjh_atguigu@atguigu.com>
2020-07-06 15:51:33 +0800

    edit by hot_fix

--- src/main/java/git/Hello.java
+++ src/main/java/git/Hello.java
@@ -5,6 +5,6 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("abcxxxx");
 		System.out.println("xxx");
-		System.out.println("hotfix");
+		System.out.println("hotfix...");
 	}
 }
